f13f26d63ca9a1eb79c5416fa9dee13339d8034f
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-01-06 16:10:06 +0530

    Finish Testing and Implementing Contact.java and ContactTest.java

--- src/main/java/org/grandstrandsystems/Contact.java
+++ src/main/java/org/grandstrandsystems/Contact.java
@@ -25,50 +25,76 @@ public class Contact {
     private String address;
 
 
-    public Contact(String id, String firstName, String lastName, String phoneNumber, String address) {
+    public Contact(String id, String firstName, String lastName, String phoneNumber, String address) throws IllegalArgumentException {
+        setId(id);
         setFirstName(firstName);
+        setLastName(lastName);
+        setPhoneNumber(phoneNumber);
+        setAddress(address);
     }
 
-    public void setFirstName(String firstName) throws IllegalArgumentException {
-        if (firstName.length() > FIRST_NAME_MAX_LENGTH)
-            throw new IllegalArgumentException("First Name cannot exceed " + FIRST_NAME_MAX_LENGTH + ".");
-        this.firstName = firstName;
-    }
+    /**
+     * *******************************************
+     * *               GETTERS                   *
+     * *******************************************
+     */
 
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
+    public String getId() {
+        return id;
     }
 
     public String getFirstName() {
         return firstName;
     }
 
-
-
-    public String getId() {
-        return id;
+    public String getLastName() {
+        return lastName;
     }
 
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
 
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     * *******************************************
+     * *               SETTERS                   *
+     * *******************************************
+     */
+
+    public void setId(String id) throws IllegalArgumentException {
+        if(id.length() > ID_MAX_LENGTH)
+            throw new IllegalArgumentException("ID cannot exceed " + ID_MAX_LENGTH + " character(s).");
+        if (this.id != null)
+            throw new IllegalArgumentException("Cannot mutate ID after instantiation.");
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) throws IllegalArgumentException {
+        if (firstName.length() > FIRST_NAME_MAX_LENGTH)
+            throw new IllegalArgumentException("First Name cannot exceed " + FIRST_NAME_MAX_LENGTH + " character(s).");
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) throws IllegalArgumentException {
+        if (lastName.length() > LAST_NAME_MAX_LENGTH)
+            throw new IllegalArgumentException("Last name cannot exceed " + LAST_NAME_MAX_LENGTH + " character(s).");
+        this.lastName = lastName;
+    }
+
+    public void setPhoneNumber(String phoneNumber) throws IllegalArgumentException {
+        if(phoneNumber.length() > PHONE_NUMBER_MAX_LENGTH || phoneNumber.length() < PHONE_NUMBER_MAX_LENGTH)
+            throw new IllegalArgumentException("Phone Number must be " + PHONE_NUMBER_MAX_LENGTH + " characters long.");
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setAddress(String address) throws IllegalArgumentException {
+        if (address.length() > ADDRESS_MAX_LENGTH)
+            throw new IllegalArgumentException("Address cannot exceeed " + ADDRESS_MAX_LENGTH + " character(s).");
+        this.address = address;
+    }
 
 }
--- src/test/java/org/grandstrandsystems/ContactTest.java
+++ src/test/java/org/grandstrandsystems/ContactTest.java
@@ -22,9 +22,9 @@ class ContactTest {
     private static String[] invalidPhoneNumbers = {"1234", "8", "12341234111", "98881888888"};
 
 
-    @Tag("First Name Tests")
     @Test
-     void firstName_oneCharacterAboveLimit_ThrowsException() {
+    @Tag("FirstName")
+     void firstName_oneCharacterAboveLimit_throwsException() {
         StringBuilder firstName = new StringBuilder();
         byte limit = Contact.FIRST_NAME_MAX_LENGTH + 1;
         for (byte i = 0; i < limit; i++) {
@@ -35,9 +35,9 @@ class ContactTest {
         });
     }
 
-    @Tag("First Name Tests")
     @Test
-    void firstName_veryLong_ThrowsException() {
+    @Tag("FirstName")
+    void firstName_veryLong_throwsException() {
         StringBuilder firstName = new StringBuilder();
         Random random = new Random();
         int limit = Contact.FIRST_NAME_MAX_LENGTH + random.nextInt(100) + 10; // Exceeds the string limit from 10 to 110
@@ -49,13 +49,137 @@ class ContactTest {
         });
     }
 
-    @Tag("First Name Tests")
     @Test
-    void firstName_inLimit_ThrowsException() {
+    @Tag("FirstName")
+    void firstName_inLimit_createsObjectWithCorrectFirstName() {
         String[] validFirstNames = {"a", "first name", "FirstName", "First Name"};
         for(String firstName: validFirstNames) {
-            Contact contact = new Contact("001", firstName, "Last Name", "123467890", "Some Address");
+            Contact contact = new Contact("001", firstName, "Last Name", "1234567890", "Some Address");
             assertEquals(firstName, contact.getFirstName());
         }
     }
+
+    @Test
+    @Tag("LastName")
+    void lastName_oneCharacterAboveLimit_throwsException() {
+        StringBuilder lastName = new StringBuilder();
+        byte limit = Contact.LAST_NAME_MAX_LENGTH + 1;
+        for (byte i = 0; i < limit; i++) {
+            lastName.append("a");
+        }
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Contact("001", "First Name", lastName.toString(), "1234567890", "Some Address");
+        });
+    }
+
+    @Test
+    @Tag("LastName")
+    void lastName_veryLong_throwsException() {
+        StringBuilder lastName = new StringBuilder();
+        Random random = new Random();
+        int limit = Contact.LAST_NAME_MAX_LENGTH + random.nextInt(100) + 10; // Exceeds the string limit from 10 to 110
+        for (byte i = 0; i < limit; i++) {
+            lastName.append("a");
+        }
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Contact("001", "First Name", lastName.toString(), "1234567890", "Some Address");
+        });
+    }
+
+    @Test
+    @Tag("LastName")
+    void lastName_inLimit_createsObjectWithCorrectLastName() {
+        String[] validLastNames = {"a", "last name0", "LastName0", "Last Name0"};
+        for(String lastName: validLastNames) {
+            Contact contact = new Contact("001", "First Name", lastName, "1234567890", "Some Address");
+            assertEquals(lastName, contact.getLastName());
+        }
+    }
+
+    @Test
+    @Tag("PhoneNumber")
+    void phoneNumber_greaterThanLimit_throwsException() {
+        StringBuilder phoneNumber = new StringBuilder();
+        Random random = new Random();
+        int limit = random.nextInt(25) + Contact.PHONE_NUMBER_MAX_LENGTH + 1; // Bound between 1 and 26 character above phone limit
+        phoneNumber.append("0".repeat(limit));
+        assertThrows(IllegalArgumentException.class, () -> new Contact("001", "First Name", "Last Name", phoneNumber.toString(), "Some Address"));
+
+    }
+
+    @Test
+    @Tag("PhoneNumber")
+    void phoneNumber_lessThanLimit_throwsException() {
+        StringBuilder phoneNumber = new StringBuilder();
+        Random random = new Random();
+        int limit = random.nextInt(Contact.PHONE_NUMBER_MAX_LENGTH - 1); // Bound between 0 and PHONE_NUMBER_MAX_LENGTH - 1
+        phoneNumber.append("0".repeat(limit));
+        assertThrows(IllegalArgumentException.class, () -> new Contact("001", "First Name", "Last Name", phoneNumber.toString(), "Some Address"));
+    }
+
+    @Test
+    @Tag("PhoneNumber")
+    void phoneNumber_inLimit_createsObjectWithCorrectPhoneNumber() {
+        StringBuilder phoneNumber = new StringBuilder();
+        phoneNumber.append("0".repeat(Contact.PHONE_NUMBER_MAX_LENGTH));
+        Contact contact = new Contact("001", "First Name", "Last Name", phoneNumber.toString(), "Some Address");
+        assertEquals(phoneNumber.toString(), contact.getPhoneNumber());
+    }
+
+    @Test
+    @Tag("Address")
+    void address_greaterThanLimit_throwsException() {
+        StringBuilder addresss = new StringBuilder();
+        Random random = new Random();
+        int limit = random.nextInt(25) + Contact.ADDRESS_MAX_LENGTH + 1; // Bound between 1 and 26 character above address limit
+        addresss.append("a".repeat(limit));
+        assertThrows(IllegalArgumentException.class, () -> new Contact("001", "First Name", "Last Name","1234567890", addresss.toString() ));
+
+    }
+
+    @Test
+    @Tag("Address")
+    void address_inLimit_createsObjectWithCorrectAddress() {
+        StringBuilder address = new StringBuilder();
+        Random random = new Random();
+        int limit = random.nextInt(Contact.ADDRESS_MAX_LENGTH); // Bound between 0 and ADDRESS_MAX_LENGTH - 1
+        address.append("a".repeat(limit));
+        Contact contact = new Contact("001", "First Name", "Last Name","1234567890", address.toString());
+        assertEquals(address.toString(), contact.getAddress());
+    }
+
+    @Test
+    @Tag("ID")
+    void id_greaterThanLimit_throwsException() {
+        StringBuilder id = new StringBuilder();
+        Random random = new Random();
+        int limit = random.nextInt(25) + Contact.ID_MAX_LENGTH + 1; // Bound between 1 and 26 character above address limit
+        id.append("0".repeat(limit));
+        assertThrows(IllegalArgumentException.class, () -> new Contact(id.toString(), "First Name", "Last Name","1234567890", "Some Address"));
+
+    }
+
+    @Test
+    @Tag("ID")
+    void id_inLimit_createsObjectWithCorrectID() {
+        StringBuilder id = new StringBuilder();
+        Random random = new Random();
+        int limit = random.nextInt(Contact.ID_MAX_LENGTH); // Bound between 0 and ADDRESS_MAX_LENGTH - 1
+        id.append("0".repeat(limit));
+        Contact contact = new Contact(id.toString(), "First Name", "Last Name","1234567890", "Some Address");
+        assertEquals(id.toString(), contact.getId());
+    }
+
+    @Test
+    @Tag("ID")
+    void id_mutatedAfterInstantiation_throwsException() {
+        String oldId = "000001";
+        String newId = "000001";
+        Contact contact = new Contact(oldId, "First Name", "Last Name","1234567890", "Some Address");
+
+        assertEquals(oldId, contact.getId());
+
+        assertThrows(IllegalArgumentException.class, () -> contact.setId(newId));
+    }
+
 }
