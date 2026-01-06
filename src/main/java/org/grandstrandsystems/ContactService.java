89666830ee713944be6b7a1d8c3462d645afe14f
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-01-06 16:41:40 +0530

    Add ContactService BoilerPlate code + Tests

--- src/main/java/org/grandstrandsystems/ContactService.java
+++ src/main/java/org/grandstrandsystems/ContactService.java
@@ -1,9 +1,6 @@
 package org.grandstrandsystems;
 
 /**
- * This class represents a user's contact.
- * Each contact have a First Name, Last Name, Phone Number, Address and a Unique ID
- * It provides constructors overloads for instantiation with all or a combination of parameters.
  *
  * @author Alan Abraham Puthenparambil Kochumon
  * @version 1.0
@@ -12,16 +9,20 @@ package org.grandstrandsystems;
 
 public class ContactService {
 
-    static final byte ID_MAX_LENGTH = 10;
-    static final byte FIRST_NAME_MAX_LENGTH = 10;
-    static final byte LAST_NAME_MAX_LENGTH = 10;
-    private static final byte PHONE_NUMBER_MAX_LENGTH = 10;
-    private static final byte ADDRESS_MAX_LENGTH = 30;
+    void addContact(String firstName, String lastName, String phoneNumber, String address) {
 
+    }
 
-    private String id;
-    private String firstName;
-    private String lastName;
-    private String phoneNumber;
-    private String address;
+    void deleteContact(String id) {
+
+    }
+
+    void updateContact(String id, String firstName, String lastName, String phoneNumber, String address) {
+
+    }
+
+    Contact getContact(String id) {
+        // TODO(Not Implemented)
+        return null;
+    }
 }
---
+++ src/test/java/org/grandstrandsystems/ContactServiceTest.java
@@ -0,0 +1,7 @@
+package org.grandstrandsystems;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ContactServiceTest {
+
+}
